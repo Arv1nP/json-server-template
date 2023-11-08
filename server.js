da5a185a77e3a08b3ea8b93e5fd41236e899fba3
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-11-08 17:27:42 +1100

    Server

--- server.js
+++ server.js
@@ -8,20 +8,6 @@ const middlewares = jsonServer.defaults();
 server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
-
-// Create custom routes for specific resources
-server.get("/ratings", (req, res) => {
-  // Implement the logic to return ratings data from your JSON file
-  const ratings = require(path.join(__dirname, "db", "db.json")).ratings;
-  res.json({ ratings });
-});
-
-server.get("/lifeQuotes", (req, res) => {
-  // Implement the logic to return lifeQuotes data from your JSON file
-  const lifeQuotes = require(path.join(__dirname, "db", "db.json")).lifeQuotes;
-  res.json({ lifeQuotes });
-});
-
 server.use(router);
 
 const PORT = process.env.PORT || 3001;
